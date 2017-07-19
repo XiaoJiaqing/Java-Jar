65c0e02e392fb66042bc32508021facc7783667e
ziv <937211946@qq.com>
2017-07-19 16:45:37 +0800

    first commit

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MyClass {
+
+    public static int getInt() {
+        return 0;
+    }
+
+    public static void main(String[] var0) {
+        System.out.println("main "+getInt());
+    }
+}
