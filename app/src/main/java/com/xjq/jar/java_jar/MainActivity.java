0ece6c043cd80ec7ef8fe90a2f4d97701ce5d403
ziv <937211946@qq.com>
2017-07-19 16:53:44 +0800

    test

---
+++ app/src/main/java/com/xjq/jar/java_jar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xjq.jar.java_jar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
